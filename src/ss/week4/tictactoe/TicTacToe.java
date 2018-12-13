ede12be96eb61387e02b0bba3c41843858bf999b
Marije Linthorst <marije.linthorst@nedap.com>
2018-12-13 14:31:59 +0100

    Add main method (assigment P-4.13)

--- src/ss/week4/tictactoe/TicTacToe.java
+++ src/ss/week4/tictactoe/TicTacToe.java
@@ -1,5 +1,4 @@
 package ss.week4.tictactoe;
-
 /**
  * Executable class for the game Tic Tac Toe. The game can be played against the
  * computer. Lab assignment Module 2
@@ -9,6 +8,9 @@ package ss.week4.tictactoe;
  */
 public class TicTacToe {
     public static void main(String[] args) {
-        // TODO: implement, see P-4.21
+      Player s0 = new HumanPlayer (args[0], Mark.XX);
+      Player s1 = new HumanPlayer (args[1], Mark.OO);
+        Game game = new Game(s0, s1);
+        game.start();   
     }
 }
